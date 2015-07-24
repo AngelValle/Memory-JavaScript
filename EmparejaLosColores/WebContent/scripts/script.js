18112f5584cc6eef858f5bebd700f5082a6fb1d6
AngelValle <angel.sine.leganes@gmail.com>
2015-07-25 01:19:34 +0200

    Actualizacion JavaScript

--- EmparejaLosColores/WebContent/scripts/script.js
+++ EmparejaLosColores/WebContent/scripts/script.js
@@ -5,7 +5,9 @@ var id_celda_seleccionada=-1;
 var pareja_celda_seleccionada=-1;
 var celdaslibres = 20;
 var array_celdas_seleccionables;
+var celda_extraida;
 
+//Metodo para construir un Color hexadecimal aleatorio
 function colorAleatorio()
 {
 	var hexadecimal = ('0123456789ABCDEF').split('');
@@ -17,6 +19,7 @@ function colorAleatorio()
 return color_aleatorio;
 }
 
+//Funcion que remuestra los Div's y oculta el input
 function mostrarCaja()
 {
 	// Recogemos el DIV_TABLA que estaba oculto que forma el juego
@@ -26,9 +29,13 @@ function mostrarCaja()
 	// Recogemos el Boton y lo ocultamos
 	var boton_rellenar = document.getElementById("Boton");
 	boton_rellenar.setAttribute("style","display:none;");
+	//AL mostrar rellenamos DIV's
 	rellenarCaja();
 }
 
+//Funcion que recoje todos los Div y los da un color aleatorio a 2 celdas extraidas de un array aleatoriamente
+//Añadimos la funcion onClick para que puedan ser procesados los eventos luego
+//Rellenamos el ARRAY GLOBAL con todos los DIVS recogidos para poder extraerlos mientras van siendo destapados
 function rellenarCaja()
 {
 	// Recogemos los DIVS que forman el juego
@@ -53,11 +60,12 @@ function rellenarCaja()
 	var Celda_19 = document.getElementById("Celda_19");
 	var Celda_20 = document.getElementById("Celda_20");
 	
+	//araya de celda a pintar
 	var array_celdas = new Array(	Celda_1,Celda_2,Celda_3,Celda_4,Celda_5,Celda_6,Celda_7,
 									Celda_8,Celda_9,Celda_10,Celda_11,Celda_12,Celda_13,
 									Celda_14,Celda_15,Celda_16,Celda_17,Celda_18,Celda_19,
 									Celda_20);
-	
+	//Las recorremos, las cogemos aleatoriamente, y le asignamos un color aleatorio
 	for ( var i = 0; i < 10; i++) 
 	{
 		var aleatorio_1 = Math.floor(Math.random()*(array_celdas.length-1));
@@ -76,29 +84,36 @@ function rellenarCaja()
 		var id_2 = celda_2.getAttribute("id");
 		celda_2.setAttribute("onclick","compararCelda("+i+","+id_2+");");
 	}
+	//Cargamos el array para poderlas despatar luego (Array de las que quedan por ser destapadas)
 	array_celdas_seleccionables = new Array(Celda_1,Celda_2,Celda_3,Celda_4,Celda_5,Celda_6,Celda_7,
 			Celda_8,Celda_9,Celda_10,Celda_11,Celda_12,Celda_13,
 			Celda_14,Celda_15,Celda_16,Celda_17,Celda_18,Celda_19,
 			Celda_20);
 }
 
+//Funcion que escucha los eventos de pulsar una celda
 function compararCelda(pareja, id)
 {
+	//Si no hay ninguna celda seleccionada
 	if(celdasseleccionadas==0)
 	{
 		var celda_seleccionada_1 = id;
 		
 		var comprobacion='False';
+		//Buscamos si la celda seleccionada ya ha sido eliminada del array de celdas por destapar
 		for (var i = 0; i < array_celdas_seleccionables.length; i++) 
 		{
 			var celda_a_comprobar = array_celdas_seleccionables[i];
+			//Si encuentra la celda en el array de celdas por destapar (Aun no se destapo)
 			if (celda_a_comprobar==celda_seleccionada_1) 
 			{
 				comprobacion = 'True';
+				//Se borra la celda del array
 				array_celdas_seleccionables.splice(i, 1);
+				celda_extraida = celda_a_comprobar;
 			}
 		}
-		
+		//Si la celda seleccionada no ha sido destapada
 		if(comprobacion=='True')
 		{
 			celdasseleccionadas=1;
@@ -108,13 +123,16 @@ function compararCelda(pareja, id)
 			id_celda_seleccionada = id;
 			pareja_celda_seleccionada = pareja;
 		}
+		//Si la celda seleccionada ya ha sido destapada
 		else
 		{
 			alert('Celda ya quitada!');
 		}
 	}
+	//SI hay alguna celda ya seleccionada
 	else if(celdasseleccionadas==1)
 	{
+		//Si la celda seleccionada es de la misma pareja Y no es la misma celda seleccionada por primera vez
 		if (pareja_celda_seleccionada==pareja && id_celda_seleccionada != id) 
 		{
 			var celda_seleccionada_1 = id_celda_seleccionada;
@@ -127,23 +145,46 @@ function compararCelda(pareja, id)
 			celda_seleccionada_2.style.backgroundColor = "#FFF";
 			celda_seleccionada_2.style.opacity = "1";
 			
+			//Buscamos si la celda seleccionada ya ha sido eliminada del array de celdas por destapar
+			for (var i = 0; i < array_celdas_seleccionables.length; i++) 
+			{
+				var celda_a_comprobar = array_celdas_seleccionables[i];
+				if (celda_a_comprobar==celda_seleccionada_2) 
+				{
+					//Se borra la celda del array
+					array_celdas_seleccionables.splice(i, 1);
+				}
+			}
+			
+			
 			celdasseleccionadas=0;
 			id_celda_seleccionada=-1;
 			pareja_celda_seleccionada=-1;
 			celdaslibres = celdaslibres-2;
+			//Si ya se acabaron todas las celdas por destapar rellenamos el tablero de nuevo
 			if(celdaslibres==0)
 			{
 				alert("Enhorabuena!! Has ganado");
+				//Establecemos valor inicial a 20 (20 Casillas)
 				celdaslibres = 20;
+				//Rellenamos Caja
 				rellenarCaja();
 			}
 		}
+		//Sino es la misma pareja o es la misma celda que pulsamos la primera vez
 		else
 		{
 			celdasseleccionadas=0;
 			
 			var celda_seleccionada_1 = id_celda_seleccionada;
 			celda_seleccionada_1.style.opacity = "1";
+			
+			if (celda_extraida!=null) 
+			{
+				array_celdas_seleccionables.splice(0, 0, celda_extraida);
+				celda_extraida = null;
+			}
+			
 			
 			id_celda_seleccionada=-1;
 			pareja_celda_seleccionada=-1;
